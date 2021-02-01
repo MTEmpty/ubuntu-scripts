8c4f3118abd25b2d224536e37afef74fcdb3646e
MTmtEmpty <45623263+MTEmpty@users.noreply.github.com>
2021-02-01 21:50:10 +1000

    Create setup_server.sh

---
+++ setup_server.sh
@@ -0,0 +1,16 @@
+sudo apt-get update
+
+sudo apt-get -y install git
+
+sudo apt-get -y install htop
+
+sudo apt-get -y install neofetch
+
+sudo apt-get -y install ranger
+
+sudo apt-get -y install net-tools
+
+sudo apt-get -y install openssh-server
+# Possibly might want to run these manually as well
+# sudo ufw allow ssh
+# sudo systemctl enable sshd
