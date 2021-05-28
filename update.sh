201c26a982b7307da475e4d8771a89b54fe4b913
MTmtEmpty <45623263+MTEmpty@users.noreply.github.com>
2021-05-28 23:32:26 +1000

    add example cron to update

--- update.sh
+++ update.sh
@@ -4,4 +4,5 @@ apt update
 apt upgrade -y
 apt autoremove -y
 
-# 0 18 * * 5 cron for 18:00 every friday
+# Update every friday
+# 0 18 * * 5 /bin/bash /home/username/repos/ubuntu-scripts/update.sh > /var/log/autoupdate.log 2>&1
