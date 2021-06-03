7b971a15adb62c2c506ae8741b6925d2796a157a
MTmtEmpty <45623263+MTEmpty@users.noreply.github.com>
2021-06-03 22:56:50 +1000

    change to use apt-get

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-apt update
-apt upgrade -y
-apt autoremove -y
+apt-get update
+apt-get upgrade -y
+apt-get autoremove -y
 
 # Update every friday
 # 0 18 * * 5 /bin/bash /home/username/repos/ubuntu-scripts/update.sh > /var/log/autoupdate.log 2>&1
