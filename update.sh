8f51b4c3b2df85861a0ffffa3e3b0b97551cb1c1
MTmtEmpty <45623263+MTEmpty@users.noreply.github.com>
2021-06-14 23:15:23 +1000

    add useful print at end

--- update.sh
+++ update.sh
@@ -4,5 +4,9 @@ apt-get update -qq
 apt-get -q -y -o Dpkg::Use-Pty=0 upgrade
 apt-get autoremove -qq
 
+# Print out time it ran
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+echo "Finished updated at $dt"
+
 # Update every friday
 # 0 18 * * 5 /bin/bash /home/username/repos/ubuntu-scripts/update.sh > /var/log/autoupdate.log 2>&1
